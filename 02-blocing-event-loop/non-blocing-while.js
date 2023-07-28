027da9af0b1a97b27fbac3d70586773780c5020d
Danil <125916970+w-develop-w@users.noreply.github.com>
2023-07-28 22:45:33 +0300

    non-blocing-while

---
+++ 02-blocing-event-loop/non-blocing-while.js
@@ -0,0 +1,21 @@
+const fs = require("fs")
+let isRunning = true
+
+setTimeout(() => isRunning = false, 100)
+process.nextTick(() => console.log("Next tick"))
+
+function setImmediatePromise() {
+    return new Promise((resolve, reject) => {
+        setImmediate(() => resolve())
+        // with resolve() we stay on the same event loop iteration
+    })
+}
+
+async function whileLoop() {
+    while (isRunning) {
+        console.log("While loop is running...")
+        await setImmediatePromise()
+    }
+}
+
+whileLoop().then(() => console.log("While loop endedx"))
